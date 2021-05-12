625d8b4bafe727cf1b0c49e8e6d68d52297e6a58
Unai Aguilera <unai.aguilera@deusto.es>
2021-05-12 09:40:15 +0200

    Modificaciones necesarias para uso de Docker

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -13,7 +13,7 @@ import java.net.URI;
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
+    public static final String BASE_URI = "http://0.0.0.0:8080/myapp/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
@@ -35,11 +35,13 @@ public class Main {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-        final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.stop();
+        try {
+            startServer();
+            System.out.println(String.format("Jersey app started with WADL available at "
+                    + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+            Thread.currentThread().join();
+        } catch (Exception e) {
+            System.out.println("There was an error while starting the server.");
+        }
     }
 }
-
